8126a6eea66d6a26b65d638c2fc89b2be7fc3de1
TOPwisitwst <loktokdk_@hotmail.com>
2023-12-25 00:31:39 +0700

    first commit

--- src/app/page1/page1.component.spec.ts
+++ src/app/page1/page1.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { Page1Component } from './page1.component';
 
 describe('Page1Component', () => {
--- src/app/page1/page1.component.ts
+++ src/app/page1/page1.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { NgForm } from '@angular/forms';
+
+
+
 
 
 @Component({
@@ -10,35 +12,91 @@ import { NgForm } from '@angular/forms';
 })
 export class Page1Component  implements OnInit{
 
-  newData: any = {};
-  value:any;
 
-  formData = {
-    name: '',
-    email: ''  };
+  name:any;
+  currentDate: Date;
+  startDate: string = '2020-12-24';
+  daysDiff: number = 0;
+  formattedDateDiff: string = '';
+  showMessage: boolean = false;
+  showMusic: boolean = true;
+  points: number = 0;
+  welcome: boolean = true;
+  textchange: string = "ต้องตอบคำถามก่อนนะ ถึงจะไปต่อได้ คะแนนน้อยก็ไปต่อไม่ได้นะ แบร่";
+  keeppoint: number = 0;
 
-  constructor( private http: HttpClient ){}
+
+  
+  
+    
+
+  constructor( private http: HttpClient ){
+    this.currentDate = new Date();
+    const date1 = new Date(this.startDate);
+    const date2 = new Date();
+    const timeDiff = date2.getTime() - date1.getTime();
+    this.daysDiff = Math.floor(timeDiff / (1000 * 3600 * 24));
+    this.formattedDateDiff = daysToYearsAndMonths(this.daysDiff);
+    
+    }
 
   ngOnInit(){
     
+
   }
 
-  // addData() {
-  //   const groupData = this.newData;
-  //   this.http.post('/assets/data.json', groupData).subscribe(
-  //     response => {
-  //       console.log('Data saved:', response);
-  //     },
-  //      error => {
-  //       console.error('Error saving data:', error);
-  //     });
-  // }
 
-  // submitForm(form: NgForm) {
-  //   if (form.valid) {
-  //     console.log('Form submitted!', this.formData);
-  //   }
-  // }
-  
+  openMessage() {
+    if (this.name && this.name.trim() !== '') {
+      this.showMessage = true;
+      this.welcome = false;
+
+    } else {
+      alert('Please enter your name !')
+    }
+  }
+
+  hidemusic() {
+    this.showMusic = !this.showMusic;
+  }
+
+  onAnswerSelected(pointsToAdd: number) {
+    this.points += pointsToAdd;
+  }
+
+  submitForm() {
+    // Add your form submission logic here
+    console.log('Form submitted !');
+    console.log(this.points);
+
+    this.keeppoint = this.points;
+
+    if (this.points > 7) {
+      this.textchange = "เยี่ยมมาก เค้ารู้มันง่ายสำหรับเธอ !!";
+    }
+
+    if (this.points  < 5) {
+      this.textchange = "ตอบผิดเยอะไปรึเปล่าเนี่ยย ไออ้วน ตั้งใจหน่อยฮะ !!!";
+    }
+    
+  }
+
+ 
+
 
 }
+
+function daysToYearsAndMonths(days: number): string {
+  const years = Math.floor(days / 365);
+  const months = Math.floor((days % 365) / 30);
+
+  if (years === 0) {
+    return `${months} month${months !== 1 ? 's' : ''}`;
+  } else if (months === 0) {
+    return `${years} year${years !== 1 ? 's' : ''}`;
+  } else {
+    return `${years} year${years !== 1 ? 's' : ''} ${months} month${months !== 1 ? 's' : ''}`;
+  }
+}
+
+
